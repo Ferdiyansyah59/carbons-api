328e839cad1e8e39baeb229192fd72e8111ffb47
Ferdiyansyah59 <ferdir5599@gmail.com>
2024-06-14 23:42:08 +0700

    port

--- app.py
+++ app.py
@@ -67,5 +67,5 @@ def test():
     return 'Hallow ddd'
 
 if __name__ == "__main__":
-    app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
+    app.run(port=int(os.environ.get("PORT", 8017)),host='0.0.0.0',debug=True)
 
